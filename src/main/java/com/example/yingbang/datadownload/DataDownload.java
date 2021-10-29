ac866894c479541cbe419b346745e61e10f6523b
eternalnext <93337275+eternalnext@users.noreply.github.com>
2021-10-29 16:16:03 +0800

    Add files via upload

--- src/main/java/com/example/yingbang/datadownload/DataDownload.java
+++ src/main/java/com/example/yingbang/datadownload/DataDownload.java
@@ -45,9 +45,6 @@ public class DataDownload {
         mapTitle.put("Cookie", "bid=QNoG_zn4mZY; _pk_id.100001.4cf6=6209709719896af7.1575619506.2.1575940374.1575621362.; __utma=30149280.1889677372.1575619507.1575619507.1575940335.2; __utmz=30149280.1575619507.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utma=223695111.986359939.1575619507.1575619507.1575940335.2; __utmz=223695111.1575619507.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __yadk_uid=QVSP2uvzzDBrpnvHKzZpZEWJnuARZ4aL; ll=\"118259\"; _vwo_uuid_v2=D1FC45CAE50CF6EE38D245C68D7CECC4F|e8d1db73f4c914f0b0be7ed85ac50d14; trc_cookie_storage=taboola%2520global%253Auser-id%3D690a21c0-9ad9-4f8d-b997-f0decb3cfc9b-tuct4e39874; _pk_ses.100001.4cf6=*; ap_v=0,6.0; __utmb=30149280.0.10.1575940335; __utmc=30149280; __utmb=223695111.0.10.1575940335; __utmc=223695111; __gads=ID=2f06cb0af40206d0:T=1575940336:S=ALNI_Ma4rv9YmqrkIUNXsIt5E7zT6kZy2w");
         //获取前100条数据，可以自行更改
         for(int i = 0; i < 360; i+=20){
-            if( i != 0 && i % 40 ==0){
-                Thread.sleep(60000);
-            }
             map.put("page_start", i+"");
             String html = HttpUtils.doGetHtml(url, map, mapTitle);
             if(html == ""){
@@ -56,6 +53,7 @@ public class DataDownload {
             JSONObject jsonObject = JSONObject.parseObject(html);
             JSONArray jsonArray = jsonObject.getJSONArray("subjects");
             for(int j = 0; j < jsonArray.size(); j++){  //循环遍历每页数据
+                Thread.sleep(6000);
                 Movie movie = new Movie();
                 JSONObject json = (JSONObject) jsonArray.get(j);
                 int id = json.getInteger("id");
